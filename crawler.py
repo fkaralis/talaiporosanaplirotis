96a1f6e5f624608ff1021f3a7d5404633c27aac6
Panos Mavrogiorgos <pmav99@gmail.com>
2016-10-24 22:27:53 +0300

    Remove trailing whitespace.
    Trailing whitespace is considered bad practice. You can set your editor
    to automatically remove it whenever you save the file
    
    http://softwareengineering.stackexchange.com/questions/121555/why-is-trailing-whitespace-a-big-deal

--- crawler.py
+++ crawler.py
@@ -16,31 +16,33 @@ if __name__ == "__main__":
     year = str(sys.argv[1])
     school_year = year + '-' + str(int(year) + 1)
     globalvars.suffix = '/index' + year + '.html'
-  
+
     # create folder
     if not os.path.exists(school_year):
         try:
             os.makedirs(school_year)
         except OSError as exc: # Guard against race condition (me: ??)
             if exc.errno != errno.EEXIST:
-                raise   
-    
+                raise
+
     # create log file
     log_filename = school_year + '/log ' + school_year + '.txt'
     globalvars.log = open(log_filename, 'w')
     msg = 'Crawling through school year ' + school_year + '\n'
     print(msg.rstrip())
     globalvars.log.write(msg)
-    
 
     # parse initial url
     url = 'http://e-aitisi.sch.gr'
     if year != '2016':
         url += globalvars.suffix
     parse_url(url)
-    
+
     # end output
     msg = '\nDone\n\nFound ' + str(globalvars.links_count) + ' links and ' + str(globalvars.tables_count) + ' tables'
     globalvars.log.write(msg)
     print(msg)
-    
+
+
+if __name__ == "__main__":
+    year = str(sys.argv[1])
--- parser.py
+++ parser.py
@@ -7,33 +7,33 @@ import requests
 import globalvars
 
 def parse_link(url, tag):
-    
+
     filetypes = ['.xls', '.xlsx', '.csv']
 
-    # (entirely useless) fix // in middle of url 
+    # (entirely useless) fix // in middle of url
     url = re.sub(r'^((?:(?!//).)*//(?:(?!//).)*)//', r'\1/', url)
-    
+
     if any(url.endswith(x) for x in filetypes):
         filename = url.rsplit('/')[-1]
         msg = 'Found excel table: ' + filename + ' ' + url + ' ' + str(tag.contents) + '\n'
         globalvars.tables_count += 1
         globalvars.log.write(msg)
-    
-    
+
+
     elif url.endswith('gz'):
         filename = url.rsplit('/')[-1]
         msg = 'Found gz table: ' + filename + ' ' + url + ' ' + str(tag.contents) + '\n'
         globalvars.tables_count += 1
         globalvars.log.write(msg)
-    
-        
+
+
     elif url.endswith('.html') and 'index' not in url:
         filename = url.rsplit('/')[-1]
         msg = 'Found html table: ' + filename + ' ' + url + ' ' + str(tag.contents) + '\n'
         globalvars.tables_count += 1
         globalvars.log.write(msg)
 
-            
+
     elif ('index' in url and 'old' not in url) or url.endswith('/'):
         msg = '------------------\nFound link: ' + url + ' ' + str(tag.contents) + '\n'
         globalvars.log.write(msg)
@@ -42,8 +42,8 @@ def parse_link(url, tag):
             parse_url(url)
         else:
             globalvars.log.write('Crazy 2007/2011 link to 2016/2013 index\n')
-        
-        
+
+
     else:
         msg = '--Not xls, html, or gz ' + url + ' ' + str(tag.contents) + '\n'
         globalvars.log.write(msg)
@@ -55,12 +55,12 @@ def create_url(url, href):
     return url + '/' + href
 
 
-def fix_url(url, suffix):  
+def fix_url(url, suffix):
     if suffix in url:       # initial index,html
         list = url.split(suffix)
-        url = ''.join(list)        
+        url = ''.join(list)
 
-    rogue_suffixes = ['/indexAB.html', '/indexC.html', '/indexG.html', 
+    rogue_suffixes = ['/indexAB.html', '/indexC.html', '/indexG.html',
                            '/indexABg.html', '/indexCg.html', '/indexGg.html']
     if any(x in url for x in rogue_suffixes):       # 2003-4
         splitter = re.search('/index.+?\.html', url).group(0)
@@ -74,7 +74,7 @@ def fix_url(url, suffix):
         splitter = re.search('/indexdior.html', url).group(0)
         list = url.split(splitter)
         url = ''.join(list)
-    
+
     return url
 
 
@@ -82,8 +82,8 @@ def parse_url(url):
     html = requests.get(url)
     html.encoding = 'ISO-8859-7'
     soup = BeautifulSoup(html.content, 'html.parser')
-    
-    # fix .html suffixes in middle of url  
+
+    # fix .html suffixes in middle of url
     url = fix_url(url, globalvars.suffix)
 
     tags = soup('a')
@@ -91,4 +91,4 @@ def parse_url(url):
         href = tag.get('href')
         if not re.match('index\d+\.html', href):        # end 2016
             link_url = create_url(url, href)
-            parse_link(link_url, tag) 
+            parse_link(link_url, tag)
