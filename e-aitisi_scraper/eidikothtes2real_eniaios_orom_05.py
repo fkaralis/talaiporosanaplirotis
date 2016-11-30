ed201ef708820ca1556e4cffc45e77af72ef5f2f
fkaralis <fivoskaralis@gmail.com>
2016-11-30 20:55:49 +0200

    scripting pinakes eidikothta (eniaios orom 05) into db

--- e-aitisi_scraper/eidikothtes2real_eniaios_orom_05.py
+++ e-aitisi_scraper/eidikothtes2real_eniaios_orom_05.py
@@ -43,29 +43,9 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def match(eidikothta, real_eidikothta):
-    print("Assigning %s %s to Eidikothta %s",
-                real_eidikothta.kodikos_real_eidikothtas, real_eidikothta.lektiko_real_eidikothtas,
-                eidikothta.kodikos_eidikothtas)
-    eidikothta.real_eidikothta_id = real_eidikothta.id
-    session.commit()
 
-real_eidikothtes = session.query(Real_eidikothta).all()
-eidikothtes = session.query(Eidikothta).filter_by(real_eidikothta_id=0).all()
-kladoi = session.query(Klados).all()
 pinakes = session.query(Pinakas).filter_by(eidikothta_id = 1439).all()
-'''
-count_eid = 0
-for eidikothta in eidikothtes:
-    count_eid += 1
 
-    eidikothta_id = eidikothta.id
-    kodikos_eidikothtas = eidikothta.kodikos_eidikothtas
-
-    #logger.info('--------\n %s %s %s', count_eid, eidikothta_id, kodikos_eidikothtas)
-
-    pinakes = session.query(Pinakas).filter_by(eidikothta_id=eidikothta_id).all()
-'''
 
 count = 0
 for pinakas in pinakes:
@@ -96,34 +76,20 @@ for pinakas in pinakes:
     except Exception as e:
         klados = session.query(Klados).filter_by(kodikos_kladoy = lektiko_pinaka[:-3]).one()
 
-    klados_real_eidikothta_id = klados.real_eidikothta_id
+    real_eidikothta_id = klados.real_eidikothta_id
     kodikos_k = klados.kodikos_kladoy
     lektiko_k = klados.lektiko_kladoy
 
-    real_eidikothta = session.query(Real_eidikothta).filter_by(id = klados_real_eidikothta_id).one()
+    real_eidikothta = session.query(Real_eidikothta).filter_by(id = real_eidikothta_id).one()
     kodikos_re = real_eidikothta.kodikos_real_eidikothtas
     lektiko_re = real_eidikothta.lektiko_real_eidikothtas
 
-    logger.info("%s %s %s %s %s %s", lektiko_pinaka, letters_pinaka,
-                kodikos_k, lektiko_k, kodikos_re, lektiko_re)
+    eidikothta = session.query(Eidikothta).filter_by(real_eidikothta_id = real_eidikothta_id).first()
+
+    logger.info("%s %s %s %s %s", lektiko_pinaka,
+                kodikos_k, lektiko_k, kodikos_re, lektiko_re, eidikothta.kodikos_eidikothtas)
 
 
-
-
-'''
-
-            letters_pinaka = re.search('(\D+)\d*', lektiko_pinaka_parts[0]).group(1)
-            first_pinaka = re.search('\D+(\d+)', lektiko_pinaka_parts[0].split('.')[0]).group[1]
-
-            logger.info('%s %s %s', letters_pinaka, first_pinaka, last_pinaka)
-'''
-
-
-
-
-        #print('Eidikothta', eidikothta.kodikos_eidikothtas, 'not MOYSIKH!!')
-        #eidikothta.real_eidikothta_id = 16
-        #session.commit()
 
 
 
--- e-aitisi_scraper/eidikothtes2real_moysika.py
+++ e-aitisi_scraper/eidikothtes2real_moysika.py
@@ -49,7 +49,6 @@ def match(eidikothta, real_eidikothta):
     eidikothta.real_eidikothta_id = real_eidikothta.id
     session.commit()
 
-#real_eidikothtes = session.query(Real_eidikothta).all()
 real_eidikothtes = session.query(Real_eidikothta).all()
 eidikothtes = session.query(Eidikothta).filter_by(real_eidikothta_id=0).all()
 kladoi = session.query(Klados).all()
@@ -80,74 +79,3 @@ for eidikothta in eidikothtes:
 
 
 
-
-
-
-    '''
-
-    #re pattern
-    p = re.compile(r'_|\.|,+')
-    eidikothta_parts = p.split(kodikos_eidikothtas)
-
-    try:
-        letters_eidikothtas = re.search('(\D+)\d*', eidikothta_parts[0]).group(1).upper()
-    except Exception as e:
-        letters_eidikothtas = None
-
-    # for PE, DE, TE
-    if len(letters_eidikothtas) < 3:
-        try:
-            numbers_eidikothtas = []
-            for i in eidikothta_parts:
-                numbers_eidikothtas.append(re.search('\D*(\d*)', i).group(1))
-                # remove ''
-                numbers_eidikothtas = [x for x in numbers_eidikothtas if x != '']
-        except Exception as e:
-            print('IN NUMBERS_EIDIKOTHTAS LIST')
-
-        try:
-            first_eidikothtas = numbers_eidikothtas[0]
-        except Exception as e:
-            first_eidikothtas = None
-
-        try:
-            last_eidikothtas = numbers_eidikothtas[1]
-        except Exception as e:
-            last_eidikothtas = None
-
-        print('--------------\n', count, eidikothta_id, kodikos_eidikothtas,
-              letters_eidikothtas, numbers_eidikothtas,
-              first_eidikothtas, last_eidikothtas)
-
-        for klados in kladoi:
-            kodikos_kladoy = klados.kodikos_kladoy
-            letters_kladoy = re.search('(\D+)\d+\.*\d*\D*', kodikos_kladoy).group(1)
-            if letters_kladoy == 'ΠΕ':
-                letters_kladoy = 'PE'
-            elif letters_kladoy == 'ΤΕ':
-                letters_kladoy = 'TE'
-            elif letters_kladoy == 'ΔΕ':
-                letters_kladoy = 'DE'
-
-            if letters_eidikothtas == letters_kladoy:
-                klados_id = klados.id
-                lektiko_kladoy = klados.lektiko_kladoy
-                real_eidikothta_id_kladoy = klados.real_eidikothta_id
-
-                number_kladoy = re.search('\D*(\d+\.*\d*)\D*', kodikos_kladoy).group(1)
-                parts_number_kladoy = number_kladoy.split('.')
-                first_kladoy = parts_number_kladoy[0]
-                if first_eidikothtas == first_kladoy:
-                    if len(parts_number_kladoy) > 1:
-                        last_kladoy = parts_number_kladoy[1]
-
-                        if last_eidikothtas == last_kladoy:
-                            print(kodikos_kladoy, lektiko_kladoy, 'match')
-                            real_eidikothta = session.query(Real_eidikothta).filter_by(id=real_eidikothta_id_kladoy).one()
-                            print(real_eidikothta.kodikos_real_eidikothtas,
-                                  real_eidikothta.lektiko_real_eidikothtas)
-                            match(eidikothta, real_eidikothta)
-    # for Moysika Organa, Athlimata
-    else:
-        print(letters_eidikothtas, 'mousiko organo, athlima')
-'''
