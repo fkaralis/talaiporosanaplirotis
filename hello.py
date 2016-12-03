a6f611782fbc08717d16270f83bbca664fcde897
Fivos Karalis <fivoskaralis@gmail.com>
2016-12-03 09:37:13 +0200

    flask templates

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from flask.ext.script import Manager
 
@@ -7,12 +7,11 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return('<h1>Hello World</h1><p>Your browser is %s</p>' % user_agent)
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return('<h1>Hello, %s!</h1>' % name)
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     manager.run()
