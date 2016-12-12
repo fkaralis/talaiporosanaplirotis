be18d6bf588a1d4b1c64c19ab16e1f08023c7c7c
fkaralis <fivoskaralis@gmail.com>
2016-12-13 00:26:03 +0200

    flask-sqlalchemy and db.Model

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 from flask import session
@@ -9,22 +10,92 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from datetime import datetime
 
 
-class NameForm(FlaskForm):
-    name = StringField('Όνομα', validators=[Required()])
-    submit = SubmitField('Δώσε')
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'e-aitisi_scraper/talaiporosanaplirotis.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = '#@SCJ239asbAS<KCsdfhg7757'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+# talaiporosanaplirotis.sqlite Model
+#
+db.Model.metadata.reflect(db.engine)
+
+class Eidikothta(db.Model):
+    __table__ = db.Model.metadata.tables['eidikothta']
+
+    def __repr__(self):
+        return '\nΕιδικότητα id %r\nκωδικός %r\nreal %r' %\
+            (self.eidikothta_id, self.kodikos_eidikothtas, self.real_eidikothta_id)
+
+
+
+class Hmeromhnia(db.Model):
+    __table__ = db.Model.metadata.tables['hmeromhnia']
+
+    def __repr__(self):
+        return '\nΗμερομηνία id %r\nλεκτικό %r\nπρ. ημ/νία %r' %\
+            (self.hmeromhnia_id, self.lektiko_hmeromhnias, self.real_hmeromhnia)
+
+
+class Kathgoria(db.Model):
+    __table__ = db.Model.metadata.tables['kathgoria']
+
+    def __repr__(self):
+        return '\nΚατηγορία id %r\nλεκτικό %r' %\
+            (self.kathgoria_id, self.lektiko_kathgorias)
+
+
+class Klados(db.Model):
+    __table__ = db.Model.metadata.tables['klados']
+
+    def __repr__(self):
+        return '\nΚλάδος id %r\nκωδικός %r\nλεκτικό %r\nreal %r' %\
+            (self.klados_id, self.kodikos_kladoy, self.lektiko_kladoy, self.real_eidikothta_id)
+
+
+class Pinakas(db.Model):
+    __table__ = db.Model.metadata.tables['pinakas']
+
+    def __repr__(self):
+        return '\nΠίνακας id %r\nλεκτικό %r\nσχ.έτος %r\nκατηγορία %r\nειδικότητα %r\nημ/νια %r\npath %r\nurl %r' %\
+            (self.pinakas_id, self.lektiko_pinaka, self.sxoliko_etos_id, self.kathgoria_id, \
+             self.eidikothta_id, self.hmeromhnia_id, self.path_pinaka, self.url_pinaka)
+
+
+class Real_eidikothta(db.Model):
+    __table__ = db.Model.metadata.tables['real_eidikothta']
+
+    def __repr__(self):
+        return '\nΠραγματική ειδικότητα\nid %r\nκωδικός %r\nλεκτικό %r' %\
+            (self.real_eidikothta_id, self.kodikos_real_eidikothtas, self.lektiko_real_eidikothtas)
+
+
+class Sxoliko_etos(db.Model):
+    __table__ = db.Model.metadata.tables['sxoliko_etos']
+
+    def __repr__(self):
+        return '\nΣχολικό έτος\nid %r\nλεκτικό %r' %\
+            (self.sxoliko_etos_id, self.lektiko_sxolikoy_etoys)
+
+
+
+class NameForm(FlaskForm):
+    name = StringField('Όνομα', validators=[Required()])
+    submit = SubmitField('Δώσε')
 
 
 @app.errorhandler(404)
