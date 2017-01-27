4035e57c07da354b8997bbabdb8de0a2bec72838
fkaralis <fivoskaralis@gmail.com>
2017-01-28 00:05:10 +0200

    adding smeae_pinakas kathgoria, perioxh, mous_organo, athlima to choices and form

--- hello.py
+++ hello.py
@@ -207,13 +207,21 @@ choices_sxolika_eth = []
 choices_kathgories = []
 choices_kladoi = []
 choices_hmeromhnies = []
-choices_smeae_pinakas = [(0, '--Επιλογή πίνακα--'), (1, 'Α'), (2, 'B')]
-choices_smeae = [(0, '--Επιλογή κατηγορίας ΣΜΕΑΕ--'), (1, 'Γενικός'), (2, 'Braille'), (3, 'ΕΝΓ'), (4, 'Braille και ΕΝΓ')]
+choices_smeae_pinakes = []
+choices_smeae_kathgories = []
+choices_perioxes = []
+choices_mousika_organa = []
+choices_athlimata = []
 
 sxolika_eth = Sxoliko_etos.query.all()
 kathgories = Kathgoria.query.all()
 kladoi = Klados.query.all()
 hmeromhnies = Hmeromhnia.query.all()
+smeae_pinakes = Smeae_pinakas.query.all()
+smeae_kathgories = Smeae_kathgoria.query.all()
+perioxes = Perioxh.query.all()
+mousika_organa = Mousiko_organo.query.all()
+athlimata = Athlima.query.all()
 
 for sxoliko_etos in sxolika_eth:
     choice = (sxoliko_etos.sxoliko_etos_id, sxoliko_etos.lektiko_sxolikoy_etoys)
@@ -244,7 +252,39 @@ for i, choice in enumerate(choices_hmeromhnies):
     choice_list[1] = '{:%d-%b-%Y}'.format(choice_list[1])
     choices_hmeromhnies[i] = tuple(choice_list)
 choices_hmeromhnies.insert(0, (0, '--Επιλογή ημ/νίας--'))
+
+for smeae_pinakas in smeae_pinakes:
+    choice = (smeae_pinakas.id, smeae_pinakas.lektiko)
+    choices_smeae_pinakes.append(choice)
+choices_smeae_pinakes = sorted(choices_smeae_pinakes, key=lambda x: x[1]) # sort alphabetically
+choices_smeae_pinakes.insert(0, (0, '--Επιλογή πίνακα ΣΜΕΑΕ--'))
+
+for smeae_kathgoria in smeae_kathgories:
+    choice = (smeae_kathgoria.id, smeae_kathgoria.lektiko)
+    choices_smeae_kathgories.append(choice)
+choices_smeae_kathgories.insert(0, (0, '--Επιλογή κατηγορίας ΣΜΕΑΕ--'))
+
+for perioxh in perioxes:
+    choice = (perioxh.id, perioxh.lektiko)
+    choices_perioxes.append(choice)
+choices_perioxes = sorted(choices_perioxes, key=lambda x: x[1]) # sort alphabetically
+choices_perioxes.insert(0, (0, '--Επιλογή περιοχής--'))
+
+for mousiko_organo in mousika_organa:
+    choice = (mousiko_organo.id, mousiko_organo.lektiko)
+    choices_mousika_organa.append(choice)
+choices_mousika_organa = sorted(choices_mousika_organa, key=lambda x: x[1]) # sort alphabetically
+choices_mousika_organa.insert(0, (0, '--Επιλογή περιοχής--'))
+
+for athlima in athlimata:
+    choice = (athlima.id, athlima.lektiko)
+    choices_athlimata.append(choice)
+choices_athlimata = sorted(choices_athlimata, key=lambda x: x[1]) # sort alphabetically
+choices_athlimata.insert(0, (0, '--Επιλογή αθλήματος--'))
+
 ## end choices
+
+
 
 
 # form class
@@ -260,11 +300,11 @@ class Form(FlaskForm):
                             coerce=int,\
                             id='select_kathgoria')
     smeae_pinakas = SelectField('Πίνακας ΣΜΕΑΕ',\
-                                choices=choices_smeae_pinakas,\
+                                choices=choices_smeae_pinakes,\
                                 coerce=int,\
                                 id='select_smeae_pinakas')
-    smeae = SelectField('Κατηγορία για ΣΜΕΑΕ',\
-                        choices=choices_smeae,\
+    smeae_kathgoria = SelectField('Κατηγορία για ΣΜΕΑΕ',\
+                        choices=choices_smeae_kathgories,\
                         coerce=int,\
                         id='select_smeae')
     klados = SelectField('Κλάδος',\
@@ -276,6 +316,21 @@ class Form(FlaskForm):
                              validators=[DataRequired()],\
                              coerce=int,\
                              id='select_hmeromhnia')
+    perioxh = SelectField('Περιοχή',\
+                             choices=choices_perioxes,\
+                             validators=[DataRequired()],\
+                             coerce=int,\
+                             id='select_perioxh')
+    mousiko_organo = SelectField('Mουσικό όργανο',\
+                             choices=choices_mousika_organa,\
+                             validators=[DataRequired()],\
+                             coerce=int,\
+                             id='select_mousiko_organo')
+    athlima = SelectField('Άθλημα',\
+                             choices=choices_athlimata,\
+                             validators=[DataRequired()],\
+                             coerce=int,\
+                             id='select_athlima')
     submit = SubmitField('Yποβολή')
 #
 # form end
@@ -302,13 +357,13 @@ def index():
         sxoliko_etos_id = form.sxoliko_etos.data
         kathgoria_id = form.kathgoria.data
         if form.smeae_pinakas.data:
-            smeae_pinakas = form.smeae_pinakas.data
+            smeae_pinakas_id = form.smeae_pinakas.data
         else:
-            smeae_pinakas = 0
-        if form.smeae.data:
-            smeae = form.smeae.data
+            smeae_pinakas_id = 0
+        if form.smeae_kathgoria.data:
+            smeae_kathgoria_id = form.smeae_kathgoria.data
         else:
-            smeae = 0
+            smeae_kathgoria_id = 0
         klados_id = str(form.klados.data)
         hmeromhnia_id = form.hmeromhnia.data
         real_eidikothta_id = Klados.query.filter_by(klados_id=klados_id).first().real_eidikothta_id
@@ -321,8 +376,8 @@ def index():
 
         session['sxoliko_etos_id']  = sxoliko_etos_id
         session['kathgoria_id'] = kathgoria_id
-        session['smeae_pinakas'] = smeae_pinakas
-        session['smeae'] = smeae
+        session['smeae_pinakas_id'] = smeae_pinakas_id
+        session['smeae_kathgoria_id'] = smeae_kathgoria_id
         session['klados_id'] = klados_id
         session['hmeromhnia_id'] = hmeromhnia_id
         session['real_eidikothta_id'] = real_eidikothta_id
@@ -331,8 +386,8 @@ def index():
         if app.config['TALAIPANAP_ADMIN']:
             msg_body = '\n'.join(('Σχολικό έτος id ' + str(sxoliko_etos_id),\
                                   'Κατηγορία id ' + str(kathgoria_id),\
-                                  'Πίνακας ΣΜΕΑΕ ' + str(smeae_pinakas),\
-                                  'ΣΜΕΑΕ: ' + str(smeae),\
+                                  'Πίνακας ΣΜΕΑΕ id' + str(smeae_pinakas_id),\
+                                  'Κατηγορία ΣΜΕΑΕ id: ' + str(smeae_kathgoria_id),\
                                   'Κλάδος id ' + str(klados_id),\
                                   'Ημερομηνία id ' + str(hmeromhnia_id),\
                                   'Real ειδικότητα id ' + str(real_eidikothta_id),\
@@ -348,8 +403,8 @@ def index():
 def result():
     return render_template('result.html', sxoliko_etos_id=session.get('sxoliko_etos_id'),
                            kathgoria_id=session.get('kathgoria_id'),
-                           smeae_pinakas=session.get('smeae_pinakas'),
-                           smeae=session.get('smeae'),
+                           smeae_pinakas_id=session.get('smeae_pinakas_id'),
+                           smeae_kathgoria_id=session.get('smeae_kathgoria_id'),
                            klados_id=session.get('klados_id'),
                            hmeromhnia_id=session.get('hmeromhnia_id'),
                            real_eidikothta_id=session.get('real_eidikothta_id'),
