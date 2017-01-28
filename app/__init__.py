38987b2956d3afaa3dac7b88721d71f286ed52a0
fkaralis <fivoskaralis@gmail.com>
2017-01-28 22:13:23 +0200

    app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
